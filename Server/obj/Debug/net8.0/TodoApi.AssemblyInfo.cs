c8dc094366f947bcd51b5a6be641499d1ceac9a6
הודיה טולדנו <a0547006431@gmail.com>
2025-03-23 18:54:33 +0200

    treurhwbrir

--- Server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783b7bfba2428befac8065f4990755bbf3c77666")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+808a9f34a083a5dfe0fe43077f8ae224c375ab93")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
